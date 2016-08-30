42adc8d5428c7c6f6baaafaab699f6b3008dd266
Diego Alberto Barriga Martínez <umoqnier@gmail.com>
2016-08-30 02:37:04 +0200

    Primer commit
    Creación de la rama develop y primer commit

---
+++ complementos.py
@@ -0,0 +1,8 @@
+#Universidad Nacional Autonoma de México/ Facultad de Ingeniería
+#Lenguajes de programación
+#Alumnos:
+#-Banda Martínez César Eduardo
+#-Barriga Martínez Diego Alberto
+#-Quezada Tapia Abraham
+#Programa: Operaciones de polinomios de grado N
+#Hecho en Python 3.6
